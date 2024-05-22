9e82cc41509cc3a871fbfc198cd4316d70af1487
Naman Khater <naman22khater@gmail.com>
2024-05-22 10:38:09 +0530

    base application setup

---
+++ utils/texts/serverBasicResponse.js
@@ -0,0 +1,9 @@
+class ServerBasicResponse {
+    HEALTH_CHECK_RESPONSE = 'Server is up and running';
+    SERVER_STARTED_MESSAGE = 'Server is running on port';
+
+    DATABASE_CONNECTED_MESSAGE = 'Database connected successfully';
+    DATABASE_CONNECTION_ERROR = 'Error connecting to database';
+}
+
+module.exports = ServerBasicResponse;
